b9c2a9d8e21a1d01e88e913dfe5fe9473a4f2966
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-11-15 16:22:36 -0600

    Adds vignette

--- Assets/Scripts/CharacterScripts/PlayerStats.cs
+++ Assets/Scripts/CharacterScripts/PlayerStats.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+//using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStats : CharacterStats
 {
-    [SerializeField] private Image fog;
+    [SerializeField] private VolumeProfile vp;
 
+    private Vignette _vignette;
     protected float _restStat = 10, _restMaxStat = 10;
     protected int _checkpointIndex = 0;
+
+    private void OnEnable()
+    {
+        if (vp) vp.TryGet(out _vignette);
+    }
 
     public void IncreaseRestStat(float increaseAmount) {
         _restStat = Mathf.Clamp(_restStat + increaseAmount, 0, _restMaxStat);
 
-        if (fog)
+        if (_vignette)
         {
-            fog.color = new Vector4(fog.color.r, fog.color.b, fog.color.g, 1 - (_restStat / _restMaxStat));
+            _vignette.intensity.value = 1 - (_restStat / _restMaxStat);
         }
     }
 }
