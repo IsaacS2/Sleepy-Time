85b3b8685cbd1b140b365f020bdba576748e5be1
Thomas James Timusk <84927705+tjtimusk@users.noreply.github.com>
2024-11-18 21:26:00 -0500

    Weapon swing implemented

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     //TODO
-    //Checkpoint Choices, or yse both, one for checkpoint, one for level clear
+    //Checkpoint Choices, or use both, one for checkpoint, one for level clear
     //AkSoundEngine.PostEvent("Play_SFX_Checkpoint_Distorted_Chime_Stinger", gameObject);
     //AkSoundEngine.PostEvent("Play_SFX_Checkpoint_String_Stingerr", gameObject);
     //End
