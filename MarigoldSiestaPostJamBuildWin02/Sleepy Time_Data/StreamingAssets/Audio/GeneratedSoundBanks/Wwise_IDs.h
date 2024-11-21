ad28759728e44cd03e7123e8485582ffe447e1a9
Thomas James Timusk <84927705+tjtimusk@users.noreply.github.com>
2024-11-21 17:45:42 -0500

    This is the stuff from me trying to make a windows build

---
+++ MarigoldSiestaPostJamBuildWin02/Sleepy Time_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,100 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB = 2959533290U;
+        static const AkUniqueID PLAY_AMB_SLEEP_TEST = 768061573U;
+        static const AkUniqueID PLAY_DEATH_SEQUENCE = 4163297412U;
+        static const AkUniqueID PLAY_DGX_CHILD_BARKS = 2579943048U;
+        static const AkUniqueID PLAY_DGX_ENEMY_AGGRO = 3614410221U;
+        static const AkUniqueID PLAY_DGX_ENEMY_DEATH = 3733121397U;
+        static const AkUniqueID PLAY_DGX_ENEMY_INJURED = 4287381608U;
+        static const AkUniqueID PLAY_DGX_ENTITY = 1819158867U;
+        static const AkUniqueID PLAY_DGX_ENTITY_DRY = 3940443801U;
+        static const AkUniqueID PLAY_DGX_PLAYER_FALL = 3634809319U;
+        static const AkUniqueID PLAY_DGX_PLAYER_HURT = 512385751U;
+        static const AkUniqueID PLAY_DGX_PLAYER_SCREAM = 1911911943U;
+        static const AkUniqueID PLAY_DGX_PLAYER_SNORING = 1950652224U;
+        static const AkUniqueID PLAY_DGX_PLAYER_WAKE = 800539628U;
+        static const AkUniqueID PLAY_DGX_SPIRIT_INJURED = 1459153067U;
+        static const AkUniqueID PLAY_MUS_GAME = 3188041860U;
+        static const AkUniqueID PLAY_MUS_TITLE = 3867386064U;
+        static const AkUniqueID PLAY_MUS_WIN = 4102914192U;
+        static const AkUniqueID PLAY_SFX_CHECKPOINT_DISTORTED_CHIME_STINGER = 3303012769U;
+        static const AkUniqueID PLAY_SFX_CHECKPOINT_STRING_STINGER = 3414352583U;
+        static const AkUniqueID PLAY_SFX_ENEMY_ATTACK = 2788169225U;
+        static const AkUniqueID PLAY_SFX_FS_PLAYER = 1601566421U;
+        static const AkUniqueID PLAY_SFX_RESAMPLED_VOICE_STINGER = 1765240081U;
+        static const AkUniqueID PLAY_SFX_SWIPE_UP_STINGER = 1936094923U;
+        static const AkUniqueID PLAY_SFX_UI_SPLATTER = 793446718U;
+        static const AkUniqueID PLAY_SFX_WEAPON_IMPACT = 2499893359U;
+        static const AkUniqueID PLAY_SFX_WEAPON_SWING = 1374858245U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace SLEEPING
+        {
+            static const AkUniqueID GROUP = 1313188810U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ASLEEP = 3778954319U;
+                static const AkUniqueID AWAKE = 1151176110U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TITLE = 3705726509U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace SLEEPING
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DROWSINESS = 2455975320U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID DGX = 412430282U;
+        static const AkUniqueID MUS = 712897226U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID DGX = 412430282U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUS = 712897226U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AUX_VERB = 1952219607U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
